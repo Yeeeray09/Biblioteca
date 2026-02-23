4e2c21f8cfed4afe4f3faf102a588f438b563514
Gabriel Sánchez <gabrielsanchzz06@gmail.com>
2026-02-23 16:26:28 +0000

    He añandido el constructor y hecho la vista, tanto la consola como el main

--- src/controlador/GestorBiblioteca.java
+++ src/controlador/GestorBiblioteca.java
@@ -1,4 +1,35 @@
 package controlador;
+
+import modelo.Usuario;
+import modelo.libro;
+import modelo.prestamo;
+import vista.consola;
+import java.util.ArrayList;
+
 public class GestorBiblioteca {
+    private ArrayList<libro> inventario;
+    private ArrayList<Usuario> usuarios;
+    private ArrayList<prestamo> registroGlobalPrestamos;
+    private consola vista;
+
+    //Constructor del Controlador 
     
+    public GestorBiblioteca(consola vista) {
+        // Inicializamos las listas del Modelo
+        this.inventario = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+        this.registroGlobalPrestamos = new ArrayList<>();
+        
+        // Inyectamos la vista
+        this.vista = vista;
+        
+        this.vista.mostrarMensaje("Sistema iniciado: Gestión de Libros, Usuarios e Historiales lista.");
+    }
+
+    // Método para añadir un usuario 
+    public void registrarUsuario(int id, String nombre) {
+        Usuario nuevo = new Usuario(id, nombre);
+        this.usuarios.add(nuevo);
+        this.vista.mostrarMensaje("Usuario '" + nombre + "' registrado con ID: " + id);
+    }
 }
--- src/vista/consola.java
+++ src/vista/consola.java
@@ -1,5 +1,31 @@
 package vista;
 
+import java.util.Scanner;
+
 public class consola {
-    
+    private Scanner sc;
+
+    public consola() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(">> " + mensaje);
+    }
+
+    public String leerTexto(String etiqueta) {
+        System.out.print(etiqueta + ": ");
+        return sc.nextLine();
+    }
+
+    public int leerEntero(String etiqueta) {
+        System.out.print(etiqueta + ": ");
+        while (!sc.hasNextInt()) {
+            System.out.println("Error: Introduce un número válido.");
+            sc.next();
+        }
+        int numero = sc.nextInt();
+        sc.nextLine(); 
+        return numero;
+    }
 }
